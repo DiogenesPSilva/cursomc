9b9a9dd7a709d4a7d84a843c2ccb8e8f95c1e44a
Diógenes Silva <diogenes.fdl@gmail.com>
2019-05-28 14:14:52 -0300

    Criação do mapeamento da Entidade Produto e associação muitos para muitos

--- src/main/java/com/diogenes/cursomc/CursomcApplication.java
+++ src/main/java/com/diogenes/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.diogenes.cursomc.domain.Categoria;
+import com.diogenes.cursomc.domain.Produto;
 import com.diogenes.cursomc.repositories.CategoriaRepository;
+import com.diogenes.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +31,18 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
 		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().add(produto2);
+		
+		produto1.getCategorias().add(categoria1);
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().add(categoria1);
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 }
