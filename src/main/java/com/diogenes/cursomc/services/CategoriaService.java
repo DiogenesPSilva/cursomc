530a7ca15817a13c180cef4b5ea68c26c7e932f2
Diógenes Silva <diogenes.fdl@gmail.com>
2019-05-29 13:23:04 -0300

    Tratamento de erro objeto não encontrado

--- src/main/java/com/diogenes/cursomc/services/CategoriaService.java
+++ src/main/java/com/diogenes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.diogenes.cursomc.domain.Categoria;
 import com.diogenes.cursomc.repositories.CategoriaRepository;
+import com.diogenes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
 		
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Categoria.class.getName()));
 	}
 }
